55392beb469700f5b2caae2b033a4b5978be68ef
Alish Pawn <alishpau@gmail.com>
2025-03-29 17:24:09 +0545

    fix some problem

--- app/src/main/java/com/bca/mobile_programming/unit_5/Fragment_two.java
+++ app/src/main/java/com/bca/mobile_programming/unit_5/Fragment_two.java
@@ -9,10 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bca.mobile_programming.R;
+
 public class Fragment_two extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.unit_5_fragment_two, container, false);
     }
 }
